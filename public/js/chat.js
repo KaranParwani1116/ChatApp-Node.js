b4e897932c9eef8ea1e6ba36bb5bf164dd92833a
KaranParwani1116 <karanparwani.parwani102@gmail.com>
2020-10-07 13:27:39 +0530

    basic layout created

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io()
+const socket = io()
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
+
+socket.on('message', (message) => {
+    console.log(message)
+})
