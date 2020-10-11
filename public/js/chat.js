034de2cf2dcaeef07eeeddff65f9b91ddc45d12e
Karan Parwani <karanparwani.parwani102@gmail.com>
2020-10-11 10:31:18 +0530

    styling chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,7 +4,7 @@ const socket = io()
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
-const $shareLocationButton = document.querySelector('#share-location')
+const $shareLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
 //templates
